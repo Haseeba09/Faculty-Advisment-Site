3f01941e86b1acff7bd30c33717f3bac770ffe46
violentsushi <violentsushi@DESKTOP-C8EEPH5.attlocal.net>
2017-04-09 02:14:59 -0500

    Image now working properly.

--- FacultyAdvisementSite/src/java/FacultyAdvisement/ImageServlet.java
+++ FacultyAdvisementSite/src/java/FacultyAdvisement/ImageServlet.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
-@WebServlet(name = "ImageServlet", urlPatterns = {"/ImageServlet"})
+@WebServlet(name = "ImageServlet", urlPatterns = {"/faces/customerFolder/ImageServlet", "/ImageServlet"})
 public class ImageServlet extends HttpServlet {
 
     @Resource(name = "jdbc/ds_wsp")
